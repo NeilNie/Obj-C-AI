bb3e973ce912719424d927b23007d53f30f44200
Neil Nie <yongyang.nie@gmail.com>
2017-02-01 07:52:21 -0500

    Initial Commit

---
+++ Obj-C AI/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Obj-C AI
+//
+//  Created by Yongyang Nie on 2/1/17.
+//  Copyright © 2017 Yongyang Nie. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
